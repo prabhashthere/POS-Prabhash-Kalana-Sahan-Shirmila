ccfc8c2e4a04acc128966a683a95c841941ea8c0
prabhashthere <prabhashthere@gmail.com>
2017-06-30 14:03:17 +0530

    cashier

---
+++ POSSystem/Create New Bill.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace POSSystem
+{
+    partial class Create_New_Bill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Create_New_Bill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 272);
+            this.Name = "Create_New_Bill";
+            this.Text = "Create_New_Bill";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ POSSystem/Issue Loyalty Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POSSystem
+{
+    public partial class Issue_Loyalty_Card : Form
+    {
+        public Issue_Loyalty_Card()
+        {
+            InitializeComponent();
+        }
+    }
+}
